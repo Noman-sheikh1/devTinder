2fa601421456249607e2ae430369e0448da54f36
Noman-sheikh1 <maanb645@gmail.com>
2025-03-18 21:31:41 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-const express=require('express');
-const app=express();
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server i am here after nodemon");
+const express = require('express');
+const app = express();
+
+app.use("/test", (req, res,next) => {
+    console.log("Hello from the server, I am here after 1st router");
+    next();
+},(req, res,next) => {
+    console.log("Hello from the server, I am here after 2st router");
+    next();
+},(req, res,next) => {
+    console.log("Hello from the server, I am here after 3st router");
+    next();
+},(req, res,next) => {
+    console.log("Hello from the server, I am here after 4st router");
+    res.send("Response4!!!")
 });
-app.listen(7777,()=>{
-    console.log("server is successfully listening on port 3000");
+
+app.listen(7777, () => {
+    console.log("Server is successfully listening on port 7777");
 });
+
